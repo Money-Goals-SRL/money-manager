c4a300de01a0d48de502f74897e2bf62805dc736
leonardopinna <96293968+leonardopinna@users.noreply.github.com>
2023-03-29 14:15:06 +0200

    Updated TCs

---
+++ src/views/Documents/Privacy.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+function Privacy() {
+  return (
+    <>
+      <h3>
+        <strong>INFORMATIVA SUL TRATTAMENTO DEI DATI PERSONALI</strong>
+      </h3>
+      <p>
+        Money Goals SRL a socio unico, società della quale il legale
+        rappresentante è Leonardo Pinna, (di seguito Money Goals o Titolare)
+        rilascia all’Interessato o all’Utente e cioè alla persona alla quale i
+        dati personali si riferiscono, le seguenti informazioni ai sensi
+        dell’art. 13 Regolamento UE n. 2016/679 del 27 aprile 2016 (di seguito
+        GDPR), relativo alla protezione delle persone fisiche con riguardo al
+        trattamento dei dati personali. La presente informativa ha lo scopo di
+        descrivere le caratteristiche dei trattamenti svolti in relazione ai
+        dati personali forniti dall’Interessato e le misure adottate al fine di
+        tutelarne i diritti. La presente informativa viene rilasciata nel sito
+        Money Manager (https://money-goals-srl.github.io/money-manager, di
+        seguito il Sito), nella sezione dedicata alla politica sulla privacy
+        nella home-page e nel footer del sito.
+      </p>
+      <h4>Titolare del trattamento</h4>
+      <p>
+        Titolare del trattamento è Money Goals SRL a socio unico con sede legale
+        in 41014 Castelvetro di Modena (MO) in via Resistenza 3 (P.IVA
+        IT04001560368), indirizzo e-mail: moneygoalssrl@gmail.com.
+      </p>
+      <h4>
+        <strong>POLITICA SULLA PRIVACY</strong>
+      </h4>
+      <h4>Dati oggetto di trattamento</h4>
+      <p>
+        Il Titolare NON raccoglie dati sull’Utente in questo sito. Potrebbero
+        essere presenti link affiliati a terze parti, i quali prevedono un
+        tracciamento NON su questo sito, ma sul sito della terza parte alla
+        quale il link rimanda. Per ulteriori informazioni, potete inviare una
+        email a moneygoalssrl@gmail.com oppure potete leggere le informative sul
+        trattamento dei dati e le politiche dei cookies dei siti di terze parti.
+      </p>
+      <h4>Finalità del trattamento, tempo di conservazione e diffusione</h4>
+      <p>
+        I dati personali NON verranno raccolti, trattati o diffusi in alcun
+        modo.
+      </p>
+      <h4>
+        <strong>COOKIES</strong>
+      </h4>
+      <h4>Cookies utilizzati</h4>
+      <p>
+        NON sono presenti cookies o strumenti di tracciamento all’interno del
+        sito web.
+      </p>
+      <h4>Come è possibile che non abbiate cookies nel sito?</h4>
+      <p>
+        Il sito Money Manager non utilizza cookies perchè non necessita di alcun
+        tipo di dati per l’utilizzo del sito. Il sito è totalmente sviluppato da
+        noi, e abbiamo il controllo completo dello sviluppo.
+      </p>
+      <p className="document-version">Versione 01 del 28 marzo 2023</p>
+    </>
+  );
+}
+
+export default Privacy;
---
+++ src/views/Documents/TermConditions.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+
+function TermConditions() {
+  return (
+    <>
+      <h3>TERMINI E CONDIZIONI DI UTILIZZO DEL SITO</h3>
+      <p>
+        Di seguito sono descritti i termini e le condizioni alle quali Money
+        Goals SRL a socio unico offre agli utenti l’accesso agli strumenti
+        disponibili sul sito web
+        https://money-goals-srl.github.io/money-manager.
+      </p>
+      <h4>1. Definizioni</h4>
+      <p>
+        Per consentire una completa comprensione e accettazione dei presenti
+        termini e condizioni, i seguenti termini, al singolare e al plurale,
+        avranno il significato di seguito indicato: Titolare: Money Goals SRL a
+        socio unico, con sede legale in via Resistenza 3, 41014, Castelvetro di
+        Modena (MO), Italia, P.IVA IT04001560368, capitale sociale 10.000,00 €.
+        Sito: il sito web https://money-goals-srl.github.io/money-manager,
+        gestito dal Titolare, un sito informativo su calcolatori economici e
+        matematici Contenuti: i contenuti e/o servizi offerti tramite il Sito;
+        Utente: il soggetto che accede al Sito, senza distinzione di natura
+        giuridica e finalità perseguita, interessato ai Prodotti offerti tramite
+        il Sito; Consumatore: persona fisica che agisce per scopi estranei
+        rispetto all’attività imprenditoriale commerciale, artigianale o
+        professionale eventualmente svolta; Condizioni: il presente contratto
+        che disciplina i rapporti tra il Titolare e gli Utenti e la vendita o
+        l’erogazione dei Prodotti offerti tramite il Sito.
+      </p>
+      <h4>2. Stipulazione, conclusione ed efficacia delle Condizioni</h4>
+      <p>
+        Le condizioni di utilizzo del sito sono efficaci nel momento in cui un
+        utente utilizza una delle pagine del sito web. Utilizzando il sito e i
+        contenuti del sito, l’Utente accetta questi Termini e Condizioni di
+        utilizzo, dichiara di accettarne il contenuto, di esserne a conoscenza e
+        di rispettare le condizioni di utilizzo del sito. Il Titolare potrà
+        modificare o semplicemente aggiornare, in tutto o in parte, le presenti
+        Condizioni. L’Utente prende atto e accetta che eventuali modifiche alle
+        presenti Condizioni si applicheranno agli Utenti dopo la data di
+        comunicazione di modifica delle Condizioni. L’Utente è pertanto invitato
+        a visionare le Condizioni ad ogni accesso al Sito e si consiglia di
+        stamparne una copia per future consultazioni.
+      </p>
+      <h4>3. Registrazione</h4>
+      <p>Il sito non prevede la registrazione di utenti.</p>
+      <h4>4. Cancellazione account e chiusura</h4>
+      <p>Il sito non prevede la registrazione di utenti.</p>
+      <h4>5. Diritti di proprietà intellettuale</h4>
+      <p>
+        Il Titolare dichiara di essere titolare e/o licenziatario di tutti i
+        diritti di proprietà intellettuale relativi e/o afferenti al Sito e/o ai
+        materiali e contenuti disponibili sul Sito. Le presenti Condizioni non
+        concedono all’Utente alcuna licenza d’uso relativa al Sito e/o a singoli
+        Contenuti e/o materiali ivi disponibili, se non diversamente
+        disciplinato. Tutti i marchi, figurativi o nominativi e tutti gli altri
+        segni, nomi commerciali, marchi di servizio, marchi denominativi,
+        denominazioni commerciali, illustrazioni, immagini, loghi che appaiono
+        nel Sito sono e rimangono di titolarità del Titolare o dei suoi
+        licenziatari e sono protetti dalle leggi vigenti sui marchi e dai
+        relativi trattati internazionali. Eventuali riproduzioni in qualunque
+        forma dei testi esplicativi e dei contenuti del Sito, qualora non
+        autorizzate, saranno considerate violazioni del diritto proprietà
+        intellettuale ed industriale del Titolare.
+      </p>
+      <h4>6. Limitazione della Responsabilità</h4>
+      <p>
+        Il Titolare non assume alcuna responsabilità per l’eventuale uso
+        fraudolento e illecito che possa essere fatto da parte di terzi, qualora
+        dimostri di aver adottato tutte le cautele possibili in base alla
+        miglior scienza ed esperienza del momento e in base alla ordinaria
+        diligenza. L’Utente si impegna a mantenere indenne e a manlevare il
+        Titolare (nonché le eventuali società dallo stesso  controllate  o 
+        affiliate,  i  suoi  rappresentanti,  collaboratori,  consulenti, 
+        amministratori,  agenti, licenziatari, partner e dipendenti), da
+        qualsiasi obbligo o responsabilità, incluse le eventuali spese legali
+        sostenute per difendersi in giudizio, che dovessero sorgere a fronte di
+        danni provocati ad altri Utenti o a terzi, in relazione ai Contenuti
+        caricati o alla violazione dei termini di legge o dei termini delle
+        presenti Condizioni. Pertanto, il Titolare non sarà responsabile per:
+        eventuali perdite che non siano conseguenza diretta della violazione del
+        contratto da parte del Titolare; ogni perdita di opportunità commerciale
+        e qualsiasi altra perdita, anche indiretta, eventualmente subita
+        dall’Utente (quali, a titolo esemplificativo e non esaustivo, perdite
+        commerciali, perdita di ricavi, introiti, profitti o risparmi presunti,
+        perdita di contratti o di relazioni commerciali, perdita della
+        reputazione o del valore di avviamento, etc.); errato o inidoneo
+        utilizzo dell’Applicazione da parte degli Utenti o di terzi; l’emissione
+        di documenti fiscali errati a causa di errori relativi ai dati forniti
+        dall’Utente, essendo quest’ultimo l’unico responsabile del corretto
+        inserimento In nessun caso il Titolare potrà essere ritenuto
+        responsabile per una somma superiore al doppio del costo pagato
+        dall’Utente.
+      </p>
+      <h4>7. Forza maggiore</h4>
+      <p>
+        Il Titolare non potrà essere considerato responsabile per il mancato o
+        ritardato adempimento delle proprie obbligazioni, per circostanze al di
+        fuori del controllo ragionevole del Titolare dovuti ad eventi di forza
+        maggiore o, comunque, ad eventi imprevisti ed imprevedibili e, comunque,
+        indipendenti dalla volontà quali, a titolo esemplificativo e non
+        esaustivo, guasti o interruzioni alle linee telefoniche o elettriche,
+        alla rete internet e/o comunque ad altri strumenti di trasmissione,
+        indisponibilità di siti web, scioperi, eventi naturali, virus ed
+        attacchi informatici, interruzioni nell'erogazione di prodotti, servizi
+        o applicazioni di terze parti. L’adempimento delle obbligazioni da parte
+        del Titolare si intenderà sospeso per il periodo in cui si verificano
+        eventi di forza maggiore. Il Titolare compirà qualsiasi atto in suo
+        potere al fine di individuare soluzioni che consentano il corretto
+        adempimento alle proprie obbligazioni nonostante la persistenza di
+        eventi forza maggiore.
+      </p>
+      <h4>8. Collegamento a siti di terzi</h4>
+      <p>
+        Il Sito potrebbe contenere collegamenti a siti di terzi. Il Titolare non
+        esercita alcun controllo su di essi e, pertanto, non è in alcun modo
+        responsabile per i contenuti di questi siti. Alcuni di questi
+        collegamenti potrebbero rinviare a siti di terzi che forniscono servizi
+        attraverso il Sito. In questi casi, ai singoli servizi si applicheranno
+        le condizioni generali per l’uso del sito e per la fruizione del
+        servizio predisposte dai terzi, rispetto alle quali il Titolare non
+        assume alcuna responsabilità.
+      </p>
+      <h4>9. Rinuncia</h4>
+      <p>
+        Nessuna rinuncia da parte di ciascuna parte ad un articolo delle
+        presenti Condizioni sarà efficace a meno che non sia espressamente
+        dichiarato di essere una rinuncia e venga comunicata per iscritto.
+      </p>
+      <h4>10. Invalidità di singole clausole</h4>
+      <p>
+        Qualora una qualsiasi disposizione delle presenti Condizioni risultasse
+        illegale o non valida, essa non verrà considerata come parte delle
+        Condizioni e questo non influirà sulle disposizioni rimanenti che
+        continueranno ad essere valide nella misura massima consentita dalla
+        legge.
+      </p>
+      <h4>11. Privacy</h4>
+      <p>
+        La tutela e il trattamento dei dati personali avverranno in conformità
+        all'Informativa Privacy, che può essere consultata alla pagina
+        www.ideastile.it/privacy_policy
+      </p>
+      <h4>12. Legge applicabile e foro competente</h4>
+      <p>
+        Le presenti Condizioni e tutte le controversie in merito ad esecuzione,
+        interpretazione e validità sono soggette alla legge italiana e alla
+        competenza esclusiva del tribunale del luogo in cui ha sede il Titolare.
+        Qualora l’Utente sia consumatore ai sensi dell’art 3 del Codice del
+        Consumo, la competenza territoriale inderogabile è del giudice del luogo
+        di residenza o di domicilio del consumatore stesso, se ubicati nel
+        territorio dello Stato Italiano, salva la facoltà per
+        l’Utente-consumatore di adire un giudice diverso da quello del “foro del
+        consumatore” ex art. 66 bis del Codice del Consumo, competente per
+        territorio secondo uno dei criteri di cui agli artt. 18, 19 e 20 del
+        codice processuale civile.
+      </p>
+      <h4>13. Allegati ai presenti termini e condizioni</h4>
+      <p>
+        Ai presenti termini e condizioni, sono allegati due ulteriori documenti
+        che integrano e fanno parte interamente di queste condizioni:
+        <li>
+          la <strong>politica sulla privacy</strong>: questo documento specifica
+          come vengono trattate le informazioni fornite dagli utenti sul sito
+          web
+        </li>
+        <li>
+          Il <strong>disclaimer di utilizzo del sito</strong>: questo documento
+          specifica le limitazioni di responsabilità del titolare, gli obblighi
+          dell’Utente e integra le condizioni di utilizzo del sito web.
+        </li>
+        Entrambi gli allegati possono essere trovati nella home page del sito
+        (https://money-goals-srl.github.io/money-manager).
+      </p>
+      <p className="document-version">Versione 01 del 28 marzo 2023</p>
+    </>
+  );
+}
+
+export default TermConditions;
