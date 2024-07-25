4d3be3f6d3eceecdb6836709780fadff514ac949
Leonardo Pinna <96293968+leonardopinna@users.noreply.github.com>
2024-07-25 10:28:02 +0200

    Updates

---
+++ precache-manifest.c1107804a80c3b46e40bb3429ef2c171.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1692886125c68a57009e727765b14424",
+    "url": "/money-manager/index.html"
+  },
+  {
+    "revision": "f1ba6a8714ebf35d9b21",
+    "url": "/money-manager/static/js/2.4424f6da.chunk.js"
+  },
+  {
+    "revision": "9ffead6b78ebd068e46d",
+    "url": "/money-manager/static/js/main.1e0b3a98.chunk.js"
+  },
+  {
+    "revision": "3faca2b81ed603777395",
+    "url": "/money-manager/static/js/runtime~main.b3e7d9f9.js"
+  }
+]);
